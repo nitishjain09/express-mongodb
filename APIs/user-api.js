0452210b0822ea440b4bf61aba96744d40c7b2b1
Nitish <jainnitish817@gmail.com>
2021-06-18 14:55:49 +0530

    rest api with user registration and login

--- APIs/user-api.js
+++ APIs/user-api.js
@@ -1,6 +1,9 @@
 //create mini express app
 const exp = require('express');
 const userApi = exp.Router(); //creates mini exp() app obj
+const expressErrorHandler = require('express-async-handler');
+const brcyptjs = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 //add bodyparsing middleware
 userApi.use(exp.json())
@@ -37,6 +40,7 @@ userApi.get("/getUsers", (req,res) => {
 */
 
 //http://localhost:3000/user/getUsers
+/*
 userApi.get("/getUsers", (req, res, next) => {
 
     //read docs from user collection
@@ -52,9 +56,32 @@ userApi.get("/getUsers", (req, res, next) => {
         }
     })
 })
+*/
+
+
+//getUsers using promises
+/*userApi.get("/getUsers", (req, res, next) => {
+
+    userCollectionObj.find().toArray()
+        .then(usersList => { res.send({ message: usersList })})
+        .catch(err => {
+            console.log("err in reading users",err);
+            res.send({ message : err.message});
+        })
+})
+*/
+
+//getUsers using async and await
+userApi.get("/getUsers", expressErrorHandler( async (req, res, next) => {
+
+    let userList = await userCollectionObj.find().toArray();
+    res.send({ message: userList });
+}))
+
 
 //http://localhost:3000/user/getUser/<username>
-userApi.get("/getUser/:username", (req, res, next) => {
+
+/*userApi.get("/getUser/:username", (req, res, next) => {
 
     //get username from url parameter
     let un = req.params.username;
@@ -76,12 +103,53 @@ userApi.get("/getUser/:username", (req, res, next) => {
         }
     })
 })
+*/
 
 
+// get user by username using promises
+/*
+userApi.get("/getUser/:username", (req, res ,next) => {
+
+    //get username from Url
+    let un = req.params.username;
+
+    //search
+    userCollectionObj.findOne({ username: un})
+        .then(userObj => {
+            if( userObj === null ){
+                res.send({ message: 'User not found'});
+            }
+            else{
+                res.send({ message: userObj });
+            }
+        })
+        .catch(err => {
+            console.log("err in reading requested user data", err);
+            res.send({message: err.message});
+        })
+})
+*/
 
 
+// get user by username using async and await
+userApi.get("/getUser/:username", expressErrorHandler( async (req, res ,next) => {
+
+    //get username from Url
+    let un = req.params.username;
+
+    //search
+    let userObj = await userCollectionObj.findOne({ username: un });
+
+    if(userObj === null){
+        res.send({ message: 'User not existed'});
+    }
+    else{
+        res.send({ message: userObj});
+    }
+}))
 
 //http://localhost:3000/user/createUser
+/*
 userApi.post("/createUser", (req, res, next) => {
 
     //get user obj
@@ -112,6 +180,30 @@ userApi.post("/createUser", (req, res, next) => {
         }
     })
 })
+*/
+
+
+//create user
+userApi.post("/createUser", expressErrorHandler( async (req, res, next) => {
+    //get user obj
+    let newUser = req.body;
+    //search for existing user
+    let user = await userCollectionObj.findOne({ username: newUser.username});
+
+    //if user exists
+    if( user != null){
+        res.send({ message: "User already exists"});
+    }
+    else{
+        //hash password
+        let hashedPassword = await brcyptjs.hash(newUser.password, 7);
+        //replace password
+        newUser.password = hashedPassword;
+        //insert
+        await userCollectionObj.insertOne(newUser);
+        res.send({ message: 'New user created'});
+    }
+}))
 
 
 //http://localhost:3000/user/updateUser/<username>
@@ -134,8 +226,18 @@ userApi.put("/updateUser/:username", (req, res, next) => {
     })
 })
 
+userApi.put("/updateUser/:username", expressErrorHandler( async (req, res, next) => {
+    //get modified username
+    let modifiedUser = req.body;
+    //update
+    await userCollectionObj.updateOne({ username: modifiedUser.username}, { $set: {...modifiedUser}});
+    res.send({ message: 'User updated'});
+}))
+
+
 
 //http://localhost:3000/user/deleteUser/<username>
+/*
 userApi.delete("/deleteUser/:username", (req, res, next) => {
 
     //username of user to be deleted
@@ -149,11 +251,11 @@ userApi.delete("/deleteUser/:username", (req, res, next) => {
 
         try{
             userCollectionObj.deleteOne({ username: userObj.username});//, (err, success) => {
-                /*if(err){
-                    console.log("err in deleting user", err);
-                    res.send({ message: err.message});
-                }
-                else{*/
+                // if(err){
+                //     console.log("err in deleting user", err);
+                //     res.send({ message: err.message});
+                // }
+                // else{
                     res.send({ message: `User with username ${un} deleted`});
                 
             //})
@@ -163,10 +265,52 @@ userApi.delete("/deleteUser/:username", (req, res, next) => {
         }
     })
 })
+*/
 
+//delete user
+userApi.delete("/deleteUser/:username", async (req, res) => {
 
+    //get username from url
+    let un = req.params.username;
 
+    //find the user
+    let user = await userCollectionObj.findOne({ username: un });
 
+    if(user === null){
+        res.send({ message: "User does not exist"})
+    }
+    else{
+        await userCollectionObj.deleteOne({ username: un});
+        res.send({ message : 'User deleted'});
+    }
+})
+
+//user login
+userApi.post('/login', expressErrorHandler( async(req, res) => {
+
+    //get user credentials
+    let credentials = req.body;
+    //search user by username
+    let user = await userCollectionObj.findOne({ username:credentials.username});
+    //if user not found
+    if(user === null){
+        res.send({ message: 'Invalid username'});
+    }
+    else{
+        //compare the password
+        let result = await brcyptjs.compare(credentials.password, user.password);
+        //if not matched
+        if(result === false){
+            res.send({ message: "Invalid Password"});
+        }
+        else{
+            //create a token
+            let signedToken = jwt.sign({ username: credentials.username}, 'abcdef', {expiresIn: 120 });
+            //send token to client
+            res.send({ message: "login succes", token: signedToken, username: credentials.username });
+        }
+    }
+}))
 
 
 //export
